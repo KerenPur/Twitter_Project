4ea56621ce5a388a7481c1e1a80777f8a90edb11
tsofit <tsofit.bk@gmail.com>
2020-03-12 20:26:38 +0200

    saving to csv

--- collect_data.py
+++ collect_data.py
@@ -1,4 +1,5 @@
-
+import csv
+import re
 import requests
 from bs4 import BeautifulSoup
 from tweet import Tweet
@@ -15,12 +16,34 @@ def get_tweets(idle: int = 1, scrolls: int = 5):
     :param scrolls: number of scrolls we wish to simulate
     :return:
     """
+    response = ""
+    try:
+        response = requests.get(URL)
+    except requests.ConnectionError as e:
+        print("Url failure, {}".format(e))
+        exit(1)
+    scrolls = 5
 
-    response = requests.get(URL)
     soup = BeautifulSoup(response.text, 'html.parser')
     tweets = soup.findAll('li', {"class": 'js-stream-item'})
 
     return tweets
+
+
+def get_hashtag(tweet_text):
+    """
+    This function gets all the hashtags for a text in tweet
+    :param tweet_text: tweet text
+    :return: list of hashtags
+    """
+    hashtags=[]
+    tweet_text_str = str(tweet_text)
+    pattern = re.compile(r'\#([a-zA-Z\.\_\-0-9]+)')
+    matches = pattern.finditer(tweet_text_str)
+    for match in matches:
+        print(match.group(1))
+        hashtags.append(match.group(1))
+    return hashtags
 
 
 def create_tweets_obj(tweets):
@@ -37,7 +60,8 @@ def create_tweets_obj(tweets):
             replies = tweet.find('span', {"class": "ProfileTweet-actionCount"}).text.strip()
             retweets = tweet.find('span', {"class": "ProfileTweet-action--retweet"}).text.strip()
 
-            tweet_obj = Tweet(user=tweet_user, text=tweet_text, replies=replies, retweets=retweets)
+            tweet_obj = Tweet(user=tweet_user, text=tweet_text, replies=replies, retweets=retweets,
+                              hashtags=get_hashtag(tweet_text))
             tweets_dict[tweet_user] = tweet_obj
         else:
             continue
@@ -45,10 +69,34 @@ def create_tweets_obj(tweets):
     return tweets_dict
 
 
+def save_to_csv(filepath, tweets_dict):
+    """
+    This function saves the tweets dict to csv file
+    :param tweets_dict: tweets dictionary
+    :return: None
+    """
+    try:
+        with open(filepath, 'w', newline='') as myfile:
+            wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+            header = ['User', 'Content', 'Replies', 'Retweets', 'Hashtags' '\n']
+            wr.writerow(header)
+            for key in tweets_dict:
+                wr.writerow(
+                    [tweets_dict[key].user, tweets_dict[key].text, tweets_dict[key].replies, tweets_dict[key].retweets,
+                     tweets_dict[key].hashtags, '\n'])
+    except FileExistsError:
+        print('File Already exists')
+        exit(1)
+
+
 def main():
     soup_tweets = get_tweets(1,1)
     tweets_dict = create_tweets_obj(tweets=soup_tweets)
 
+    for tweet in tweets_dict:
+        print(tweets_dict[tweet])
+
+    save_to_csv('tweet.csv', tweets_dict)
 
 
 if __name__ == "__main__":
--- tweet.py
+++ tweet.py
@@ -1,5 +1,5 @@
 """
-Tweeter class, contains 4 fields
+Tweeter class, contains 5 fields
 """
 
 
@@ -8,39 +8,22 @@ class Tweet:
     initializing new tweet
     """
 
-    def __init__(self, user, text, replies, retweets):
+    def __init__(self, user, text, replies, retweets, hashtags):
         self.user = user
         self.text = text
         self.replies = replies
         self.retweets = retweets
-
-    def print_tweet(self):
-        string = """
-        Tweet Info:\n
-        user: {}\n
-        text: {}\n
-        replies: {}\n
-        Retweets: {}\n
-        """.format(self.user, self.text, self.replies, self.retweets)
-        return string
+        self.hashtags = hashtags
 
     def __str__(self):
-        string = """
-        Tweet Info:\n
-        user: {}\n
-        text: {}\n
-        replies: {}\n
-        Retweets: {}\n
-        """.format(self.user, self.text, self.replies, self.retweets)
-        return string
-
-    def __repr__(self):
-        string = """
-        Tweet Info:
+        string = """Tweet Info:
         user: {}
         text: {}
         replies: {}
         Retweets: {}
-        """.format(self.user, self.text, self.replies, self.retweets)
+        hashtags: {}
+        """.format(self.user, self.text, self.replies, self.retweets, self.hashtags)
         return string
+
+
 
