18117ce566bc4118bc505f67be660647e8d7fa54
tsofit <tsofit.bk@gmail.com>
2020-04-04 21:52:03 +0300

    adding tweet text adding hash option to identify tweet

--- tweet.py
+++ tweet.py
@@ -8,12 +8,13 @@ class Tweet:
     initializing new tweet
     """
 
-    def __init__(self, user, replies, retweets, hashtags, likes):
+    def __init__(self, user, replies, retweets, hashtags, likes, text):
         self.user = user
         self.replies = replies
         self.retweets = retweets
         self.hashtags = hashtags
         self.likes = likes
+        self.text = text
 
     def __str__(self):
         string = """Tweet Info:
@@ -22,8 +23,12 @@ class Tweet:
         Retweets: {}
         hashtags: {}
         likes: {}
-        """.format(self.user, self.replies, self.retweets, self.hashtags, self.likes)
+        text: {}
+        """.format(self.user, self.replies, self.retweets, self.hashtags, self.likes, self.text)
         return string
+
+    def __hash__(self):
+        return hash((self.text, self.user, tuple(self.hashtags)))
 
 
 
